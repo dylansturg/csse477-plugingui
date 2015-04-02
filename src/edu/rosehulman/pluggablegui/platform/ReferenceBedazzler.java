82426bf98662859dd123e5ec361ece0be535360c
Dylan Sturgeon <dylansturg@gmail.com>
2015-04-02 17:08:46 -0400

    invoking register and unregister plugins on the AWT

--- src/edu/rosehulman/pluggablegui/platform/ReferenceBedazzler.java
+++ src/edu/rosehulman/pluggablegui/platform/ReferenceBedazzler.java
@@ -1,5 +1,7 @@
 package edu.rosehulman.pluggablegui.platform;
 
+import javax.swing.SwingUtilities;
+
 import edu.rosehulman.pluggablegui.commons.IBedazzledPlatform;
 import edu.rosehulman.pluggablegui.commons.IBedazzledPlugin;
 import edu.rosehulman.pluggablegui.commons.ILogger;
@@ -7,7 +9,7 @@ import edu.rosehulman.pluggablegui.commons.ILogger;
 public class ReferenceBedazzler implements IBedazzledPlatform {
 
 	public static ReferenceBedazzler sharedInstance = new ReferenceBedazzler();
-	
+
 	private static Logger sharedLogger = new Logger();
 
 	private Renderer renderComponent;
@@ -25,19 +27,30 @@ public class ReferenceBedazzler implements IBedazzledPlatform {
 		return sharedLogger;
 	}
 
-	protected void registerPlugin(IBedazzledPlugin newPlugin) {
+	protected void registerPlugin(final IBedazzledPlugin newPlugin) {
 		// Pass on to the current renderer
 		if (this.renderComponent != null) {
-			this.renderComponent.registerPlugin(newPlugin);
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					ReferenceBedazzler.this.renderComponent.registerPlugin(newPlugin);
+				}
+			});
+
 		} else {
 			logError("Attempt to register a plugin without a Renderer");
 		}
 	}
 
-	protected void unregisterPlugin(IBedazzledPlugin removedPlugin) {
+	protected void unregisterPlugin(final IBedazzledPlugin removedPlugin) {
 		// Pass on to the current renderer
 		if (this.renderComponent != null) {
-			this.renderComponent.unregisterPlugin(removedPlugin);
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					ReferenceBedazzler.this.renderComponent.unregisterPlugin(removedPlugin);	
+				}
+			});
 		} else {
 			logError("Attempt to unregister a plugin without a Renderer");
 		}
